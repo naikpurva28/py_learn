4844807f813a9e6826db8947288504eb314010ea
naikpurva28 <naikpurva28@gmail.com>
2026-01-13 13:49:04 +0530

    Update demo.py

--- week1/demo.py
+++ week1/demo.py
@@ -1,2 +1,15 @@
 print("hii hello world")
 # print()
+def getFactorial():
+    if n<0:
+        print("factorial is not defined for negative numbers")
+        result=1
+        for i in range(1,n+1):
+         result *=i
+        return result
+    def main():
+        num=int(input("enter a non-negative integer:"))
+        print(factorial(num))
+        if__name__=="__main__"
+        main()
+        
